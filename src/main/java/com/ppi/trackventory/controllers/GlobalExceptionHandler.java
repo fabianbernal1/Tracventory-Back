e6377ff0a916dc76fea0b019a2ea2b06f9697186
fabian bernal <fabian_bernal23222@elpoli.edu.co>
2025-11-15 08:29:16 -0500

    Gestion de errores

--- src/main/java/com/ppi/trackventory/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/ppi/trackventory/controllers/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.ppi.trackventory.controllers;
 
+import java.nio.file.AccessDeniedException;
+
+import javax.naming.AuthenticationException;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +22,29 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.CONFLICT)
                 .body("No se puede eliminar el registro porque está asociado a otras entidades.");
     }
+    
+    // 🔐 Usuario autenticado pero sin permiso
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> handleAccessDenied(AccessDeniedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body("Permiso denegado. No tienes acceso a este recurso.");
+    }
+
+    // 🔒 Usuario no autenticado (no logueado)
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<Object> handleAuthentication(AuthenticationException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body("Debe iniciar sesión para acceder.");
+    }
 
     // Puedes agregar más handlers para otras excepciones
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGeneralException(Exception ex) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Ocurrió un error inesperado. Contacte al administrador." + ex.getMessage());
+                .body("Ocurrió un error inesperado. Contacte al administrador. " + ex.getMessage());
     }
     
     @ExceptionHandler(BusinessException.class)
